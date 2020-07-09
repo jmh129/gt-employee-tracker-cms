cab60b9fb6b63b0f3d5a8a965c920660526cfebd
James Holding <james.holding90@gmail.com>
2020-07-09 15:04:55 -0400

    added employee roles.

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ function initPrompt() {
         "Add Department",
         "View Roles",
         "Add Roles",
-        "Update Employee Roles",
       ],
     })
     .then(function (answer) {
@@ -48,6 +47,9 @@ function initPrompt() {
           break;
         case "Add Department":
           addDapartment();
+          break;
+        case "Add Roles":
+          addRole();
           break;
       }
     });
@@ -68,13 +70,45 @@ function addDapartment() {
       message: "Please enter the new department.",
     })
     .then(function (answer) {
-      console.log(answer.name);
       connection.query(
         "INSERT INTO department(name) VALUES (?)",
         answer.name,
         function (err, res) {
           if (err) throw err;
-          console.log("Department added!");
+          console.log("Department Added!");
+        }
+      );
+    });
+}
+
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        name: "title",
+        type: "input",
+        message: "Please enter the title of the",
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "Please enter the salary for the role.",
+      },
+      {
+        name: "depID",
+        type: "input",
+        message: "Please enter the department ID.",
+      },
+    ])
+    .then(function(answer) {
+      connection.query(
+        "INSERT INTO role(title, salary, department_id) VALUES (?,?,?)",
+        [answer.title,
+        answer.salary,
+        answer.depID],
+        function (err, res) {
+          if (err) throw err;
+          console.log("Employee Role Added!");
         }
       );
     });
