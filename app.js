f09d7c943eb2888a1c2edec6b12f2f96162ca124
James Holding <james.holding90@gmail.com>
2020-07-09 10:28:08 -0400

    added schema to schema file.

---
+++ app.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "password",
+  database: "employee_DB",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log(connection.threadId);
+});
