839bec95ccadd770d303b90cce3224e55f1ef116
James Holding <james.holding90@gmail.com>
2020-07-10 09:59:32 -0400

    add employee added in and tested.

--- app.js
+++ app.js
@@ -51,6 +51,9 @@ function initPrompt() {
         case "Add Roles":
           addRole();
           break;
+        case "Add Employee":
+          addEmployee();
+          break;
       }
     });
 }
@@ -100,15 +103,49 @@ function addRole() {
         message: "Please enter the department ID.",
       },
     ])
-    .then(function(answer) {
+    .then(function (answer) {
       connection.query(
         "INSERT INTO role(title, salary, department_id) VALUES (?,?,?)",
-        [answer.title,
-        answer.salary,
-        answer.depID],
+        [answer.title, answer.salary, answer.depID],
         function (err, res) {
           if (err) throw err;
-          console.log("Employee Role Added!");
+          console.log(answer.title + "Has Been Added!");
+        }
+      );
+    });
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: "first_name",
+        type: "input",
+        message: "Please enter the first name of the employee.",
+      },
+      {
+        name: "last_name",
+        type: "input",
+        message: "Please enter the last name of the employee.",
+      },
+      {
+        name: "role_id",
+        type: "input",
+        message: "Please enter the role id.",
+      },
+      {
+        name: "manager_id",
+        type: "input",
+        message: "Please enter the ID of the employee's manager.",
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        "INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)",
+        [answer.first_name, answer.last_name, answer.role_id, answer.manager_id ],
+        function (err, res) {
+          if (err) throw err;
+          console.log(answer.first_name + " " + answer.last_name + " Has Been Added!");
         }
       );
     });
